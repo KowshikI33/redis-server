2b7ff1b44ee139b82489be88a17750ebcfc6bb13
KowshikI87 <kowshikislam8@gmail.com>
2024-05-04 19:10:15 -0400

    respond to PING

--- app/main.py
+++ app/main.py
@@ -3,13 +3,18 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
+    # Create a TCP/IP socket
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("Server is running on localhost:6379")
+
+    while True:
+        print("Waiting for a connection...")
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr} has been established.")
+
+        # Send a hardcoded PONG response as soon as the client connects
+        client_socket.sendall(b'+PONG\r\n')
+        client_socket.close()
 
 
 if __name__ == "__main__":
