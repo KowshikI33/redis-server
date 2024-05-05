7609f8795e97abf4de7a885bdbe5a9f9fa5b4e6b
KowshikI87 <kowshikislam8@gmail.com>
2024-05-04 20:55:25 -0400

    multiple message over 1 connection fixed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 import socket
 
+def handle_client(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+
+        # close connection if there is no more data
+        if not data:
+            break
+
+        client_socket.sendall(b'+PONG\r\n')
+    
 def main():
     # Create a TCP/IP socket
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
@@ -11,9 +21,10 @@ def main():
         client_socket, addr = server_socket.accept()
         print(f"Connection from {addr} has been established.")
 
-        # Send a hardcoded PONG response as soon as the client connects
-        client_socket.sendall(b'+PONG\r\n')
-        client_socket.close()
+        try:
+            handle_client(client_socket)
+        finally:
+            client_socket.close() #Socket is closed on error
 
 if __name__ == "__main__":
     main()
