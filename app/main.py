69f1e52df869c30f040b111cacceb815e3b34b29
KowshikI87 <kowshikislam8@gmail.com>
2024-05-04 22:16:59 -0400

    handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 import socket
+import threading
 
-def handle_client(client_socket):
-    while True:
-        data = client_socket.recv(1024)
+def handle_client(client_socket, addr):
+    print(f"handling connection from {addr}")
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            
+            client_socket.sendall(b'+PONG\r\n')
+    finally:
+        client_socket.close()
+        print(f"Connetion from {addr} closed")
 
-        # close connection if there is no more data
-        if not data:
-            break
-
-        client_socket.sendall(b'+PONG\r\n')
-    
 def main():
-    # Create a TCP/IP socket
+    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     print("Server is running on localhost:6379")
 
@@ -21,10 +25,10 @@ def main():
         client_socket, addr = server_socket.accept()
         print(f"Connection from {addr} has been established.")
 
-        try:
-            handle_client(client_socket)
-        finally:
-            client_socket.close() #Socket is closed on error
+        #create and start a new thread to handle this client
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+        client_thread.start()
 
+        print(f"Active connections: {threading.active_count() - 1}")
 if __name__ == "__main__":
     main()
