395ae79b92f5a0357f17d94bf73d9085b2fbe415
KowshikI87 <kowshikislam8@gmail.com>
2024-05-04 22:39:03 -0400

    added ECHO command

--- app/main.py
+++ app/main.py
@@ -8,8 +8,19 @@ def handle_client(client_socket, addr):
             data = client_socket.recv(1024)
             if not data:
                 break
-            
-            client_socket.sendall(b'+PONG\r\n')
+
+            #decode and process commands
+            decoded_data = data.decode('utf-8').strip()
+            parts = decoded_data.split('\r\n')
+            #redis is case-insensitive
+            command = parts[2].upper() 
+
+            if command == 'ECHO':
+                message = parts[4] #the command is the 5th part
+                response = f"${len(message)}\r\n{message}\r\n"
+                client_socket.sendall(response.encode()) #response.encode() --> binary format data
+            else:
+                client_socket.sendall(b'+PONG\r\n')
     finally:
         client_socket.close()
         print(f"Connetion from {addr} closed")
