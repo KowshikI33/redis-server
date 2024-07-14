a18dce4993e8a6edf39b300a83bcbc0afe158c2f
KowshikI87 <kowshikislam8@gmail.com>
2024-07-13 21:43:37 -0400

    trigger update code

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import threading
 import time
 import base64
 
-trigger_update = True #
+trigger_update = False #
 database = {} #store tuples: (value, expiry_time)
 MASTER_REPLID = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"  # Hardcoded 40-character string
 MASTER_REPL_OFFSET = 0
